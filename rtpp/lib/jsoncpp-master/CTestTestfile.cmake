269e362cb0eaf2fec3faf5a8145dee3c0826bc29
KCMac <kcmac@mac.local>
2017-11-08 11:22:03 +0800

    删除rtpp

--- rtpp/lib/jsoncpp-master/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/hgfs/vmshare/lib/jsoncpp-master
-# Build directory: /mnt/hgfs/vmshare/lib/jsoncpp-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(src)
-SUBDIRS(include)
--- rtpp/lib/jsoncpp-master/src/jsontestrunner/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/hgfs/vmshare/lib/jsoncpp-master/src/jsontestrunner
-# Build directory: /mnt/hgfs/vmshare/lib/jsoncpp-master/src/jsontestrunner
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
